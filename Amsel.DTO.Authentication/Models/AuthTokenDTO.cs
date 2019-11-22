73551d94a9c86f01dd83d190fb2a67d69b92d264
Gereon <gereonarz@outlook.com>
2019-11-22 20:11:34 +0100

    AuthService

--- Amsel.DTO.Authentication/Models/AuthTokenDTO.cs
+++ Amsel.DTO.Authentication/Models/AuthTokenDTO.cs
@@ -3,60 +3,52 @@ using Newtonsoft.Json;
 
 namespace Amsel.DTO.Authentication.Models
 {
+    public class AuthRefreshTokenDTO
+    {
+        public AuthRefreshTokenDTO(AuthTokenDTO token, AuthTokenDTO refresh)
+        {
+            Token = token ?? throw new ArgumentNullException(nameof(token));
+            Refresh = refresh ?? throw new ArgumentNullException(nameof(refresh));
+        }
+
+        public AuthTokenDTO Token { get; set; }
+        public AuthTokenDTO Refresh { get; set; }
+    }
+
     public class AuthTokenDTO
     {
         #region  CONSTRUCTORS
 
         protected AuthTokenDTO() { }
 
-        public AuthTokenDTO(string token, DateTime expireTime) {
+        [JsonConstructor]
+        public AuthTokenDTO(string token, DateTime expireTime)
+        {
             Token = token;
-            RefreshToken = null;
             ExpireTime = expireTime;
-            RefreshExpireTime = expireTime;
         }
 
-        [JsonConstructor]
-        public AuthTokenDTO(string token, string refreshToken, DateTime expireTime, DateTime refreshExpireTime) {
-            Token = token;
-            RefreshToken = refreshToken;
-            ExpireTime = expireTime;
-            RefreshExpireTime = refreshExpireTime;
-        }
 
         #endregion
 
         public DateTime ExpireTime { get; set; }
 
-        public DateTime RefreshExpireTime { get; set; }
-
-        public string RefreshToken { get; set; }
 
         public string Token { get; set; }
 
-        public bool Expired() {
+        public bool Expired()
+        {
             if (string.IsNullOrEmpty(Token))
                 return true;
 
-            return ExpireTime < DateTime.UtcNow || RefreshExpireTime < DateTime.UtcNow;
+            if (ExpireTime == default)
+                return false;
+
+            return ExpireTime < DateTime.UtcNow;
         }
 
-        public bool RefreshExpired() {
-            if (string.IsNullOrEmpty(RefreshToken))
-                return true;
-
-            return RefreshExpireTime < DateTime.UtcNow.AddMinutes(1);
-        }
-
-        public void RemoveToken() {
-            Token = null;
-        }
-
-        public bool HasRefreshToken() {
-            return RefreshToken != null && !RefreshExpired();
-        }
-
-        public bool HasToken() {
+        public bool HasToken()
+        {
             return Token != null && !Expired();
         }
     }
