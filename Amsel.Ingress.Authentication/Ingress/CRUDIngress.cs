a1e88fd588d9a89f6fc97dff86f486bd97c633d4
Gereon <gereonarz@outlook.com>
2020-01-01 16:35:48 +0100

    Tab

--- Amsel.Ingress.Authentication/Ingress/CRUDIngress.cs
+++ Amsel.Ingress.Authentication/Ingress/CRUDIngress.cs
@@ -8,6 +8,7 @@ using Amsel.Framework.Structure.Models.Address;
 using Amsel.Framework.Structure.Service;
 using Amsel.Framework.Utilities.Extentions.Http;
 using Amsel.Framework.Utilities.Extentions.Types;
+using Amsel.Resources.Authentication.Controller;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
 
@@ -17,23 +18,29 @@ namespace Amsel.Ingress.Authentication.Ingress
     {
         protected CRUDIngress(IAuthService authService) : base(authService) { }
 
-        [NotNull]
-        protected abstract APIAddress ReadAddress { get; }
+
 
         [NotNull]
-        protected abstract APIAddress InsertAddress { get; }
-
+        protected abstract string Endpoint { get; }
         [NotNull]
-        protected abstract APIAddress UpdateAddress { get; }
-
+        protected abstract string Resource { get; }
         [NotNull]
-        protected abstract APIAddress RemoveAddress { get; }
+        protected virtual APIAddress ReadAddress => new APIAddress(Endpoint, Resource, CRUDControllerResources.READ);
+        [NotNull]
+        protected virtual APIAddress InsertAddress => new APIAddress(Endpoint, Resource, CRUDControllerResources.INSERT);
+        [NotNull]
+        protected virtual APIAddress UpdateAddress => new APIAddress(Endpoint, Resource, CRUDControllerResources.UPDATE);
+        [NotNull]
+        protected virtual APIAddress RemoveAddress => new APIAddress(Endpoint, Resource, CRUDControllerResources.REMOVE);
+        [NotNull]
+        protected virtual APIAddress GetByIdAddress => new APIAddress(Endpoint, Resource, CRUDControllerResources.GET_BY_ID);
 
 
         public virtual TEntity Insert(TEntity data) { return InsertAsync(data).Result; }
 
         [NotNull]
-        public virtual async Task<TEntity> InsertAsync(TEntity data) {
+        public virtual async Task<TEntity> InsertAsync(TEntity data)
+        {
             HttpResponseMessage response = await PostAsync(InsertAddress, GetJsonContent(data)).ConfigureAwait(false);
             return await response.DeserializeElseThrowAsync<TEntity>().ConfigureAwait(false);
         }
@@ -41,25 +48,38 @@ namespace Amsel.Ingress.Authentication.Ingress
         public virtual bool Remove(TEntity data) { return RemoveAsync(data).Result; }
 
         [NotNull]
-        public virtual async Task<bool> RemoveAsync(object data) {
+        public virtual async Task<bool> RemoveAsync(object data)
+        {
             HttpResponseMessage response = await PostAsync(RemoveAddress, GetJsonContent(data)).ConfigureAwait(false);
             return response.IsSuccessStatusCode;
+        }
+
+        public virtual TEntity GetById(object id) { return GetByIdAsync(id).Result; }
+
+        [NotNull]
+        public virtual async Task<TEntity> GetByIdAsync(object id)
+        {
+            HttpResponseMessage response = await GetAsync(GetByIdAddress, new KeyValuePair<string, object>("id", id)).ConfigureAwait(false);
+            return await response.DeserializeOrDefaultAsync<TEntity>().ConfigureAwait(false);
         }
 
         public virtual TEntity Update(TEntity data) { return UpdateAsync(data).Result; }
 
         [NotNull]
-        public virtual async Task<TEntity> UpdateAsync(TEntity data) {
+        public virtual async Task<TEntity> UpdateAsync(TEntity data)
+        {
             HttpResponseMessage response = await PostAsync(UpdateAddress, GetJsonContent(data)).ConfigureAwait(false);
             return await response.DeserializeOrDefaultAsync<TEntity>().ConfigureAwait(false);
         }
 
-        public virtual (IEnumerable<TEntity> value, int? count) Read(string jsonLogicFilter = null, IEnumerable<OrderByDTO>? sort = null, int? skip = null, int? take = null) {
+        public virtual (IEnumerable<TEntity> value, int? count) Read(string jsonLogicFilter = null, IEnumerable<OrderByDTO>? sort = null, int? skip = null, int? take = null)
+        {
             return ReadAsync(jsonLogicFilter, sort, skip, take).Result;
         }
 
-        public virtual async Task<(IEnumerable<TEntity> value, int? count)> ReadAsync(string jsonLogicFilter = null, IEnumerable<OrderByDTO>? orderBy = null, int? skip = null, int? take = null) {
-            Dictionary<string, object> parameters = new Dictionary<string, object> {{nameof(skip), skip.ToString()}, {nameof(take), take.ToString()}};
+        public virtual async Task<(IEnumerable<TEntity> value, int? count)> ReadAsync(string jsonLogicFilter = null, IEnumerable<OrderByDTO>? orderBy = null, int? skip = null, int? take = null)
+        {
+            Dictionary<string, object> parameters = new Dictionary<string, object> { { nameof(skip), skip.ToString() }, { nameof(take), take.ToString() } };
             if (!jsonLogicFilter.IsNullOrEmpty())
                 parameters.Add(nameof(jsonLogicFilter), JsonConvert.SerializeObject(jsonLogicFilter));
             // if (!orderBy.IsNullOrEmpty())
--- Amsel.Ingress.Authentication/Ingress/TenantIngress.cs
+++ Amsel.Ingress.Authentication/Ingress/TenantIngress.cs
@@ -22,19 +22,22 @@ namespace Amsel.Ingress.Authentication.Ingress
 
 
         [CanBeNull]
-        public async Task<Guid?> GetIdByNameAsync(string name) {
+        public async Task<Guid?> GetIdByNameAsync(string name)
+        {
             TenantDTO tenant = await GetTenantByNameAsync(name).ConfigureAwait(false);
             return tenant.Id;
         }
 
         [NotNull]
-        public async Task<TenantDTO> GetTenantByNameAsync(string name) {
+        public async Task<TenantDTO> GetTenantByNameAsync(string name)
+        {
             KeyValuePair<string, object> nameValue = new KeyValuePair<string, object>("name", name);
             HttpResponseMessage response = await GetAsync(TenantGet, nameValue).ConfigureAwait(false);
             return await response.DeserializeElseThrowAsync<TenantDTO>().ConfigureAwait(false);
         }
 
-        public async Task<TenantDTO> GetTenantAsync(Guid id) {
+        public async Task<TenantDTO> GetTenantAsync(Guid id)
+        {
             KeyValuePair<string, object> idValue = new KeyValuePair<string, object>("id", id.ToString());
             HttpResponseMessage response = await GetAsync(TenantGet, idValue).ConfigureAwait(false);
             return await response.DeserializeElseThrowAsync<TenantDTO>().ConfigureAwait(false);
@@ -42,16 +45,14 @@ namespace Amsel.Ingress.Authentication.Ingress
 
         #region STATICS, CONST and FIELDS
 
-        protected override APIAddress ReadAddress => new APIAddress(AuthEndpointResources.ENDPOINT, AuthEndpointResources.TENANT, CRUDControllerResources.READ);
+        /// <inheritdoc />
+        protected override string Endpoint => AuthEndpointResources.ENDPOINT;
 
-        protected override APIAddress InsertAddress => new APIAddress(AuthEndpointResources.ENDPOINT, AuthEndpointResources.TENANT, CRUDControllerResources.INSERT);
-
-        protected override APIAddress UpdateAddress => new APIAddress(AuthEndpointResources.ENDPOINT, AuthEndpointResources.TENANT, CRUDControllerResources.UPDATE);
-
-        protected override APIAddress RemoveAddress => new APIAddress(AuthEndpointResources.ENDPOINT, AuthEndpointResources.TENANT, CRUDControllerResources.REMOVE);
+        /// <inheritdoc />
+        protected override string Resource => AuthEndpointResources.TENANT;
 
         [NotNull]
-        private static readonly APIAddress TenantGet = new APIAddress(AuthEndpointResources.ENDPOINT, AuthEndpointResources.TENANT, TenantControllerResources.GET);
+        private APIAddress TenantGet => new APIAddress(Endpoint, Resource, TenantControllerResources.GET);
 
         #endregion
     }
