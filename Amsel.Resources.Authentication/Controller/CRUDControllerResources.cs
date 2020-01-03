929fc2b48eda9e956baba49009153c5de92e81cb
Gereon <gereonarz@outlook.com>
2020-01-03 12:26:01 +0100

    Update CRUDControllerResources.cs

--- Amsel.Resources.Authentication/Controller/CRUDControllerResources.cs
+++ Amsel.Resources.Authentication/Controller/CRUDControllerResources.cs
@@ -3,7 +3,7 @@
     // TODO move to seperated Project
     public static class CRUDControllerResources
     {
-        public const string GET_BY_ID = "{id}";
+        public const string GET_BY_ID = "get/{id}";
         public const string READ = "read";
         public const string INSERT = "insert";
         public const string REMOVE = "remove";
