6c1cc1b58dc951419d60e03b8c1f544620b987b6
Gereon <gereonarz@outlook.com>
2020-03-17 19:11:39 +0100

    Update TenantAccess.cs

--- Amsel.Access.Authentication/Services/TenantAccess.cs
+++ Amsel.Access.Authentication/Services/TenantAccess.cs
@@ -5,7 +5,6 @@ using Amsel.Framework.Structure.Models.Address;
 using Amsel.Framework.Utilities.Extensions.Http;
 using Amsel.Resources.Authentication.Controller;
 using Amsel.Resources.Authentication.Endpoints;
-using Autofac.Features.AttributeFilters;
 using JetBrains.Annotations;
 using System;
 using System.Net.Http;
@@ -20,8 +19,8 @@ namespace Amsel.Access.Authentication.Services
         {
         }
     }
-    
-    public class TenantAccess : CRUDAccess<MultiTenantDTO>
+
+    public class TenantAccess : CRUDAccess<TenantDTO>
     {
         #region  CONSTRUCTORS
 
@@ -33,21 +32,21 @@ namespace Amsel.Access.Authentication.Services
 
         public async Task<Guid?> GetIdByNameAsync(string name)
         {
-            MultiTenantDTO tenant = await GetTenantByNameAsync(name).ConfigureAwait(false);
+            TenantDTO tenant = await GetTenantByNameAsync(name).ConfigureAwait(false);
             return tenant.Id;
         }
 
-        public async Task<MultiTenantDTO> GetTenantAsync(Guid id)
+        public async Task<TenantDTO> GetTenantAsync(Guid id)
         {
             HttpResponseMessage response = await GetAsync(TenantGet, (nameof(id), id)).ConfigureAwait(false);
-            return await response.DeserializeElseThrowAsync<MultiTenantDTO>().ConfigureAwait(false);
+            return await response.DeserializeElseThrowAsync<TenantDTO>().ConfigureAwait(false);
         }
 
         [NotNull]
-        public async Task<MultiTenantDTO> GetTenantByNameAsync(string name)
+        public async Task<TenantDTO> GetTenantByNameAsync(string name)
         {
             HttpResponseMessage response = await GetAsync(TenantGet, (nameof(name), name)).ConfigureAwait(false);
-            return await response.DeserializeElseThrowAsync<MultiTenantDTO>().ConfigureAwait(false);
+            return await response.DeserializeElseThrowAsync<TenantDTO>().ConfigureAwait(false);
         }
         #endregion
 
